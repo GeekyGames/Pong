741589d9738a39c15cf6297285e7d064c90ca757
ETBCOR <Ethan@Corgatelli-Familys-MacBook.local>
2013-09-27 15:13:17 -0600

    started player 1

--- twoplayer/script.js
+++ twoplayer/script.js
@@ -22,6 +22,11 @@ var Game = {
 		answer = Math.round(Math.sqrt(answer));
 		return answer;
 	},
+	Player1 {
+		alive: true,
+		x: 0,
+		
+	},
 	Random: function(min,max){
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
