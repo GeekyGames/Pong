70caac26ecac93410143dc58675d5d74eac6421d
Graeme Holliday <gholliday2@hotmail.com>
2013-09-29 20:07:51 -0600

    update singleplayer js file

--- singleplayer/script.js
+++ singleplayer/script.js
@@ -1,10 +1,80 @@
-var mouse = {
-	x: 0,
-	y: 0
+//Game Vars
+var paddle = {
+	x: null,
+	y: null,
+	width: null,
+	height: null,
+	color: "blue",
+	draw: function(){
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x,this.y,this.width,this.height);
+	},
+	update: function(){
+		
+	}
+}
+var ball = {
+	x: null,
+	y: null,
+	radius: 4,
+	color: "white",
+	ax: null,
+	ay: null,
+	vx: null,
+	vy: null,
+	draw: function(){
+		ctx.beginPath();
+		ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	},
+	update: function(){
+		this.ax += this.vx / Game.FPS;
+		this.ay += this.vy / Game.FPS;
+		this.x += this.vx / Game.FPS;
+		this.y += this.vy / Game.FPS;
+	}
+}
+function powerup(type){
+	this.x = null,
+	this.y = null,
+	this.type = type,
+	this.radius = 4,
+	this.ax = null,
+	this.ay = null,
+	this.vx = null,
+	this.vy = null,
+	this.draw = function(){
+		ctx.beginPath();
+		ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	},
+	this.update = function(){
+		this.ax += this.vx / Game.FPS;
+		this.ay += this.vy / Game.FPS;
+		this.x += this.vx / Game.FPS;
+		this.y += this.vy / Game.FPS;
+	}
+	switch(type){
+		case "big":
+			this.color = "red";
+			this.effect = function(){
+				switch(paddle.width){
+					case "#"://small
+						break;
+					case "#"://normal
+						break;
+					case "#"://big already
+						break;
+				}
+			};
+			break;
+	//and so on...
+	}
 }
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var rect = canvas.getBoundingClientRect();
 var Game = {
 	FPS: 20,
 	Tick: function(){
@@ -16,11 +86,6 @@ var Game = {
 	},
 	Update: function(){
 		
-	},
-	Distance: function(x,y,x2,y2){
-		var answer = (x2 - x)*(x2 - x) + (y2 - y)*(y2 - y);
-		answer = Math.round(Math.sqrt(answer));
-		return answer;
 	},
 	Random: function(min,max){
 		return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -34,17 +99,4 @@ document.onkeydown = function(e){
 document.onkeyup = function(e){
 	e = e || window.event;
 	c = e.keyCode;
-};
-document.onmousemove = function mouseMove(e){
-	e = e || window.event;
-	mouse.x = e.pageX - rect.left || e.clientX - rect.left;
-	mouse.y = e.pageY - rect.top || e.clientY - rect.top;
-};
-document.onmousedown = function mouseDown(e){
-	e = e || window.event;
-	mouse.down = true;
-};
-document.onmouseup = function mouseUp(e){
-	e = e || window.event;
-	mouse.down = false;
 };
