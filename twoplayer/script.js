1efc91a3fbca2b07c781b457ceef3f8b88de1f21
Graeme Holliday <gholliday2@hotmail.com>
2013-10-02 19:34:11 -0600

    make twoplayer basically the same as singleplayer
    try modifying this et, it should be easier :D

--- twoplayer/script.js
+++ twoplayer/script.js
@@ -1,69 +1,135 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var player1 = {
-	x: 420,
-	y: 10,
+var paddle1 = {
+	x: 250,
+	y: 624,
 	width: 75,
 	height: 10,
-	draw: function () {
-		ctx.fillStyle = "blue";
+	color: "blue",
+	draw: function(){
+		ctx.fillStyle = this.color;
 		ctx.fillRect(this.x,this.y,this.width,this.height);
 	},
-	update = function () {
+	update: function(){
 		
 	}
 }
-var player2 = {
-	x: 420,
-	y: 620,
+var paddle2 = {
+	x: 250,
+	y: 7,
 	width: 75,
 	height: 10,
-	draw = function () {
-		ctx.fillStyle = "red";
+	color: "red",
+	draw: function(){
+		ctx.fillStyle = this.color;
 		ctx.fillRect(this.x,this.y,this.width,this.height);
 	},
-	update = function () {
+	update: function(){
 		
 	}
 }
 var ball = {
-	x: 100,
-	y: 100,
-	vx: 20,
-	vy: 20,
+	x: 50,
+	y: 50,
 	radius: 6,
 	color: "white",
-	draw = function () {
+	vx: 200,
+	vy: 200,
+	draw: function(){
 		ctx.beginPath();
+		ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
 		ctx.fillStyle = this.color;
-		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
 		ctx.fill();
 	},
-	update = function () {
-		
+	update: function(){
+		this.x += this.vx / Game.FPS;
+		this.y += this.vy / Game.FPS;
+		//collision detection
+		if(this.x + this.radius > canvas.width){
+			this.vx = -this.vx;
+		}
+		if(this.x - this.radius < 0){
+			this.vx = -this.vx;
+		}
+		if(this.y + this.radius > canvas.height){
+			this.vy = -this.vy;
+		}
+		if(this.y - this.radius < 0){
+			this.vy = -this.vy;
+		}
+		//check to see if it has hit paddle
+		if(this.x - this.radius > paddle1.x && this.x + this.radius < paddle1.x + paddle1.width && this.y + this.radius > paddle1.y){
+			this.vy = -this.vy;
+		}
+		//and the other player's paddle
+		if(this.x - this.radius > paddle2.x && this.x + this.radius < paddle2.x + paddle2.width && this.y - this.radius < paddle2.y + paddle2.height){
+			this.vy = -this.vy;
+		}
 	}
 }
+function powerup(type){
+	this.x = null;
+	this.y = null;
+	this.type = type;
+	this.radius = 4;
+	this.vx = null;
+	this.vy = null;
+	this.draw = function(){
+		ctx.beginPath();
+		ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	},
+	this.update = function(){
+		this.x += this.vx / Game.FPS;
+		this.y += this.vy / Game.FPS;
+	}
+	switch(type){
+		case "big":
+			this.color = "red";
+			this.effect = function(){
+				switch(paddle.width){
+					case "#"://small
+						break;
+					case "#"://normal
+						break;
+					case "#"://big already
+						break;
+				}
+			};
+			break;
+	//and so on...
+	}
+}
+var powerup_types = ["big", "small", "bigBall", "smallBall", "iceBall", "fireBall"];//no multiball, thats too hard to implement here
+var powerups = [];
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 var Game = {
-	FPS: 20,
+	FPS: 60,
 	Tick: function(){
 		Game.Update();
 		Game.Draw();
 	},
 	Draw: function(){
-		player1.draw();
-		player2.draw();
+		paddle1.draw();
+		paddle2.draw();
 		ball.draw();
+		for(var x in powerups){
+			powerups[x].draw();
+		}
 	},
 	Update: function(){
 		ctx.clearRect(0,0,canvas.width,canvas.height);
-		player1.update();
-		player2.update();
+		paddle1.update();
+		paddle2.update();
 		ball.update();
+		for(var x in powerups){
+			powerups[x].update();
+		}
 	},
 	Random: function(min,max){
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
-}//lol this was the biggest problem; missing bracket :P
+};
 setInterval(Game.Tick,1000/Game.FPS);
 document.onkeydown = function(e){
 	e = e || window.event;
