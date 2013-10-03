49e9adbc203ff3a64977bfba03442b85b91344ec
Graeme Holliday <gholliday2@hotmail.com>
2013-10-02 19:50:20 -0600

    begin work on powerups

--- singleplayer/script.js
+++ singleplayer/script.js
@@ -88,7 +88,7 @@ function powerup(type){
 	this.x = null;
 	this.y = null;
 	this.type = type;
-	this.radius = 4;
+	this.radius = 6;
 	this.vx = null;
 	this.vy = null;
 	this.draw = function(){
@@ -100,26 +100,112 @@ function powerup(type){
 	this.update = function(){
 		this.x += this.vx / Game.FPS;
 		this.y += this.vy / Game.FPS;
+		//collision detection
+		if(this.x + this.radius > canvas.width){
+			this.vx = -this.vx;
+		}
+		if(this.x - this.radius < 0){
+			this.vx = -this.vx;
+		}
+		if(this.y + this.radius > canvas.height){
+			this.vy = -this.vy;
+		}
+		if(this.y - this.radius < 0){
+			this.vy = -this.vy;
+		}
+		//check to see if it has hit paddle
+		if(this.x - this.radius > paddle.x && this.x + this.radius < paddle.x + paddle.width && this.y + this.radius > paddle.y){
+			this.vy = -this.vy;
+			score += 1;
+			this.vx += 50 * level;
+			this.vy += 50 * level;
+		}
+		//and the enemy's paddle
+		if(this.x - this.radius > enemypaddle.x && this.x + this.radius < enemypaddle.x + enemypaddle.width && this.y - this.radius < enemypaddle.y + enemypaddle.height){
+			this.vy = -this.vy;
+		}
 	}
 	switch(type){
 		case "big":
 			this.color = "red";
 			this.effect = function(){
 				switch(paddle.width){
-					case "#"://small
+					case 50:
+						paddle.width = 75;
 						break;
-					case "#"://normal
+					case 75:
+						paddle.width = 100;
 						break;
-					case "#"://big already
+					case 100:
 						break;
 				}
-			};
+			}
 			break;
-	//and so on...
+		case "small":
+			this.color = "red";
+			this.effect = function(){
+				switch(paddle.width){
+					case 50:
+						break;
+					case 75:
+						paddle.width = 50;
+						break;
+					case 100:
+						paddle.width = 75;
+						break;
+				}
+			}
+			break;
+		case "smallBall":
+			this.color = "blue";
+			this.effect = function(){
+				switch(ball.radius){
+					case 2:
+						break;
+					case 6:
+						ball.radius = 2;
+						break;
+					case 10:
+						ball.radius = 6;
+						break;	
+				}
+			}
+			break;
+		case "bigBall":
+			this.color = "blue";
+			this.effect = function(){
+				switch(ball.radius){
+					case 2:
+						ball.radius = 6;
+						break;
+					case 6:
+						ball.radius = 10;
+						break;
+					case 10:
+						break;
+				}
+			}
+			break;
+		case "confuse":
+			this.color = "white";
+			//doesnt do anything but add another ball that is the same size as the normal one
+			//which confuses the player because they must decide between them
+			break;
+		case "iceBall":
+			this.color = "blue";
+			ball.vx /= 2;
+			ball.vy /= 2;
+			break;
+		case "fireBall":
+			this.color = "blue";
+			ball.vx *= 2;
+			ball.vy *= 2;
+			break;
 	}
 }
-var powerup_types = ["big", "small", "bigBall", "smallBall", "iceBall", "fireBall"];//no multiball, thats too hard to implement here
 var powerups = [];
+var test = new powerup("confuse");
+powerups.push(test);
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var rect = canvas.getBoundingClientRect();
