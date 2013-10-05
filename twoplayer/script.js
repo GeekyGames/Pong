6afbaa7c2ca4055c745fe0602c83edc505a56ecd
Graeme Holliday <gholliday2@hotmail.com>
2013-10-05 14:37:33 -0600

    update twoplayer

--- twoplayer/script.js
+++ twoplayer/script.js
@@ -1,96 +1,292 @@
+//Game Vars
+var mouse = {
+	x: 400,
+}
+var score = 0;
+var alive = true;
+var alerted = false;
+var paddle = {
+	x: 250,
+	y: 624,
+	width: 75,
+	height: 10,
+	color: "blue",
+	draw: function(){
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x,this.y,this.width,this.height);
+	},
+	update: function(){
+		this.x = mouse.x;
+	}
+}
+var enemypaddle = {
+	x: 250,
+	y: 7,
+	width: 75,
+	height: 10,
+	color: "red",
+	draw: function(){
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x,this.y,this.width,this.height);
+	},
+	update: function(){
+		this.x = ball.x - 37;
+	}
+}
+var ball = {
+	x: 50,
+	y: 50,
+	radius: 6,
+	color: "white",
+	vx: 300,
+	vy: 300,
+	draw: function(){
+		ctx.beginPath();
+		ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	},
+	update: function(){
+		this.x += this.vx / Game.FPS;
+		this.y += this.vy / Game.FPS;
+		//collision detection
+		if(this.x + this.radius > canvas.width){
+			this.vx = -this.vx;
+		}
+		if(this.x - this.radius < 0){
+			this.vx = -this.vx;
+		}
+		if(this.y + this.radius > canvas.height){
+			if(alerted === false){
+				alert("You failed! Your score was: "+score);
+				alerted = true;
+				alive = false;
+				ball.x = 5000;
+				paddle.y = 5000;
+				enemypaddle.x = 5000;
+				for(var x in powerups){
+					powerups[x].x = 5000;
+				}
+			}
+		}
+		if(this.y - this.radius < 0){
+			this.vy = -this.vy;
+		}
+		//check to see if it has hit paddle
+		if(this.x - this.radius > paddle.x && this.x + this.radius < paddle.x + paddle.width && this.y + this.radius > paddle.y){
+			this.vy = -this.vy;
+			this.vx += 50 * level;
+			this.vy += 50 * level;
+		}
+		//and the enemy's paddle
+		if(this.x - this.radius > enemypaddle.x && this.x + this.radius < enemypaddle.x + enemypaddle.width && this.y - this.radius < enemypaddle.y + enemypaddle.height){
+			this.vy = -this.vy;
+		}
+	}
+}
+function powerup(type){
+	this.x = Math.random() * 417 + 50;
+	this.y = Math.random() * 317 + 50;
+	this.type = type;
+	this.vx = Math.random() * 500;
+	this.vy = Math.random() * 500;
+	this.draw = function(){
+		ctx.beginPath();
+		ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	},
+	this.update = function(){
+		this.x += this.vx / Game.FPS;
+		this.y += this.vy / Game.FPS;
+		//collision detection
+		if(this.x + this.radius > canvas.width){
+			this.vx = -this.vx;
+		}
+		if(this.x - this.radius < 0){
+			this.vx = -this.vx;
+		}
+		if(this.y + this.radius > canvas.height){
+			this.vy = -this.vy;
+		}
+		if(this.y - this.radius < 0){
+			this.vy = -this.vy;
+		}
+		//check to see if it has hit paddle
+		if(this.x - this.radius > paddle.x && this.x + this.radius < paddle.x + paddle.width && this.y + this.radius > paddle.y){
+			this.x = 5000;
+			this.effect();
+		}
+		//and the enemy's paddle
+		if(this.x - this.radius > enemypaddle.x && this.x + this.radius < enemypaddle.x + enemypaddle.width && this.y - this.radius < enemypaddle.y + enemypaddle.height){
+			this.vy = -this.vy;
+		}
+	}
+	switch(type){
+		case "big":
+			this.color = "red";
+			this.radius = 6;
+			this.effect = function(){
+				switch(paddle.width){
+					case 50:
+						paddle.width = 75;
+						break;
+					case 75:
+						paddle.width = 100;
+						break;
+					case 100:
+						break;
+				}
+			}
+			break;
+		case "small":
+			this.color = "red";
+			this.radius = 6;
+			this.effect = function(){
+				switch(paddle.width){
+					case 50:
+						break;
+					case 75:
+						paddle.width = 50;
+						break;
+					case 100:
+						paddle.width = 75;
+						break;
+				}
+			}
+			break;
+		case "smallBall":
+			this.color = "blue";
+			this.radius = 6;
+			this.effect = function(){
+				switch(ball.radius){
+					case 3:
+						break;
+					case 6:
+						ball.radius = 3;
+						break;
+					case 10:
+						ball.radius = 6;
+						break;	
+				}
+			}
+			break;
+		case "bigBall":
+			this.color = "blue";
+			this.radius = 6;
+			this.effect = function(){
+				switch(ball.radius){
+					case 3:
+						ball.radius = 6;
+						break;
+					case 6:
+						ball.radius = 10;
+						break;
+					case 10:
+						break;
+				}
+			}
+			break;
+		case "confuse":
+			this.color = "white";
+			switch(ball.radius){
+				case 3:
+					this.radius = 3;
+					break;
+				case 6:
+					this.radius = 6;
+					break;
+				case 10:
+					this.radius = 10;
+					break;
+			}
+			break;
+		case "iceBall":
+			this.color = "orange";
+			this.radius = 6;
+			this.effect = function(){
+				ball.vx *= 0.75;
+				ball.vy *= 0.75;
+			}
+			break;
+		case "fireBall":
+			this.color = "orange";
+			this.radius = 6;
+			this.effect = function(){
+				ball.vx *= 1.25;
+				ball.vy *= 1.25;
+			}
+			break;
+	}
+}
+var powerups = [];
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var rect = canvas.getBoundingClientRect();
 var Game = {
-	FPS: 20,
+	FPS: 60,
 	Tick: function(){
 		Game.Update();
 		Game.Draw();
 	},
 	Draw: function(){
-		player1.draw();
-		player2.draw();
+		ctx.font = "18px Garamond";
+		ctx.fillStyle = "black";
+		ctx.fillText("Score: "+score,20,20);
+		paddle.draw();
+		enemypaddle.draw();
 		ball.draw();
+		for(var x in powerups){
+			powerups[x].draw();
+		}
 	},
 	Update: function(){
-		player1.update();
-		player2.update();
+		if(alive === true){
+			score += 1;
+		}
+		var choice = Game.Random(1,600);
+		if(choice === 1){
+			var pick = Game.Random(1,7);
+			switch(pick){
+				case 1:
+					powerups.push(new powerup("big"));
+					break;
+				case 2:
+					powerups.push(new powerup("small"));
+					break;
+				case 3:
+					powerups.push(new powerup("bigBall"));
+					break;
+				case 4:
+					powerups.push(new powerup("smallBall"));
+					break;
+				case 5:
+					powerups.push(new powerup("fireBall"));
+					break;
+				case 6:
+					powerups.push(new powerup("iceBall"));
+					break;
+				case 7:
+					for(i=1;i<=3;i++){
+						powerups.push(new powerup("confuse"));
+					}
+					break;
+			}
+		}
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+		paddle.update();
+		enemypaddle.update();
 		ball.update();
-	},
-	Distance: function(x,y,x2,y2){
-		var answer = Math.sqrt((x2 - x)*(x2 - x) + (y2 - y)*(y2 - y));
-		answer = Math.round(answer);
-		return answer;
-	},
-	player1 {
-		x: 420,
-		y: null,
-		width: null,
-		height: null,
-		draw: function () {
-			ctx.color = "blue";
-			ctx.fillRect(this.x,this.y,this.width,this.height);
-		},
-		update = function () {
-			this.x += this.vx / FPS;
-		}
-	},
-	player2 {
-		x: 420,
-		y: null,
-		width: null,
-		height: null,
-		draw = function () {
-			ctx.color = "red";
-			ctx.fillRect(this.x,this.y,this.width,this.height);
-		},
-		update = function () {
-			this.x += this.vx / FPS;
-		}
-	},
-	ball {
-		x: 100,
-		y: 100,
-		vx: 20,
-		vy: 20,
-		ax: 20,
-		ay; 20,
-		radius: 20,
-		color: "purple",
-		draw = function () {
-			ctx.beginPath();
-			ctx.fillStyle = this.color;
-			ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-		},
-		update = function () {
-			this.x += this.vx / FPS;
-			this.y += this.vy / FPS;
-			this.vx += this.ax / FPS;
-			this.vy += this.ay / FPS;
+		for(var x in powerups){
+			powerups[x].update();
 		}
 	},
 	Random: function(min,max){
 		return Math.floor(Math.random() * (max - min + 1)) + min;
-	};
+	}
+};
 setInterval(Game.Tick,1000/Game.FPS);
-document.onkeydown = function(e){
+document.onmousemove = function(e){
 	e = e || window.event;
-	c = e.keyCode;
-};
-document.onkeyup = function(e){
-	e = e || window.event;
-	c = e.keyCode;
-};
-document.onmousemove = function mouseMove(e){
-	e = e || window.event;
-	mouse.x = e.pageX - rect.left || e.clientX - rect.left;
-	mouse.y = e.pageY - rect.top || e.clientY - rect.top;
-};
-document.onmousedown = function mouseDown(e){
-	e = e || window.event;
-	mouse.down = true;
-};
-document.onmouseup = function mouseUp(e){
-	e = e || window.event;
-	mouse.down = false;
+	mouse.x = e.pageX - rect.left;
 };
