7914b8e0db6edbff6decd16b6ea5463b6bd5af5f
Graeme Holliday <gholliday2@hotmail.com>
2013-10-11 09:50:05 -0600

    huh

--- twoplayer/script.js
+++ twoplayer/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //Game Vars
 var paddle1 = {
 	x: 250,
@@ -244,11 +245,19 @@ var keys = {
 }
 var Game = {
 	FPS: 60,
+=======
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var rect = canvas.getBoundingClientRect();
+var Game = {
+	FPS: 20,
+>>>>>>> parent of 3c84a9d... what?
 	Tick: function(){
 		Game.Update();
 		Game.Draw();
 	},
 	Draw: function(){
+<<<<<<< HEAD
 		paddle1.draw();
 		paddle2.draw();
 		ball.draw();
@@ -302,16 +311,82 @@ var Game = {
 		}
 		if(keys.d === true){
 			paddle2.x += 10;
+=======
+		player1.draw();
+		player2.draw();
+		ball.draw();
+	},
+	Update: function(){
+		player1.update();
+		player2.update();
+		ball.update();
+	},
+	Distance: function(x,y,x2,y2){
+		var answer = Math.sqrt((x2 - x)*(x2 - x) + (y2 - y)*(y2 - y));
+		answer = Math.round(answer);
+		return answer;
+	},
+	player1 {
+		x: 420,
+		y: null,
+		width: null,
+		height: null,
+		draw: function () {
+			ctx.color = "blue";
+			ctx.fillRect(this.x,this.y,this.width,this.height);
+		},
+		update = function () {
+			this.x += this.vx / FPS;
+		}
+	},
+	player2 {
+		x: 420,
+		y: null,
+		width: null,
+		height: null,
+		draw = function () {
+			ctx.color = "red";
+			ctx.fillRect(this.x,this.y,this.width,this.height);
+		},
+		update = function () {
+			this.x += this.vx / FPS;
+		}
+	},
+	ball {
+		x: 100,
+		y: 100,
+		vx: 20,
+		vy: 20,
+		ax: 20,
+		ay; 20,
+		radius: 20,
+		color: "purple",
+		draw = function () {
+			ctx.beginPath();
+			ctx.fillStyle = this.color;
+			ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+		},
+		update = function () {
+			this.x += this.vx / FPS;
+			this.y += this.vy / FPS;
+			this.vx += this.ax / FPS;
+			this.vy += this.ay / FPS;
+>>>>>>> parent of 3c84a9d... what?
 		}
 	},
 	Random: function(min,max){
 		return Math.floor(Math.random() * (max - min + 1)) + min;
+<<<<<<< HEAD
 	}
 };
+=======
+	};
+>>>>>>> parent of 3c84a9d... what?
 setInterval(Game.Tick,1000/Game.FPS);
 document.onkeydown = function(e){
 	e = e || window.event;
 	c = e.keyCode;
+<<<<<<< HEAD
 	switch(c){
 		case 65:
 			keys.a = true;
@@ -326,10 +401,13 @@ document.onkeydown = function(e){
 			keys.right = true;
 			break;
 	}
+=======
+>>>>>>> parent of 3c84a9d... what?
 };
 document.onkeyup = function(e){
 	e = e || window.event;
 	c = e.keyCode;
+<<<<<<< HEAD
 	switch(c){
 		case 65:
 			keys.a = false;
@@ -345,3 +423,19 @@ document.onkeyup = function(e){
 			break;
 	}
 };
+=======
+};
+document.onmousemove = function mouseMove(e){
+	e = e || window.event;
+	mouse.x = e.pageX - rect.left || e.clientX - rect.left;
+	mouse.y = e.pageY - rect.top || e.clientY - rect.top;
+};
+document.onmousedown = function mouseDown(e){
+	e = e || window.event;
+	mouse.down = true;
+};
+document.onmouseup = function mouseUp(e){
+	e = e || window.event;
+	mouse.down = false;
+};
+>>>>>>> parent of 3c84a9d... what?
